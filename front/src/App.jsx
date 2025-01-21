4ad6a0fd6e300b60cdade8d45ce67dd5a5a73eaa
Clément Bilger <clement.bilger25@gmail.com>
2025-01-21 11:05:05 +0100

    init front
    vite w/ react & tailwindcss

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind CSS est bien configuré !
+      </h1>
+    </div>
+  );
+}
+
+export default App
